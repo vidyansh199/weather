5f4f6ca30ccca3bfb581cd70d58c28fcc4bd69f9
vidyansh199 <kvidyansh2000@gmail.com>
2023-11-23 00:21:03 +0530

    final2

--- index.js
+++ index.js
@@ -3,18 +3,12 @@ import axios from "axios";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3000;
+const port =process.env.PORT || 3000;
 const apikey="f857729bfa08878dadf48e7adc31a5a2";
 const url="https://api.openweathermap.org/data/2.5/weather";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
-
-
-
-
-
 
 app.get("/", async(req, res) => {
     const result=await axios.get(url,{
@@ -37,7 +31,7 @@ app.post("/", async(req, res) =>{
                 units:"metric"
               }
         })
-        console.log(result.data);
+        //console.log(result.data);
         res.render("index.ejs",{ location: JSON.stringify(result.data) });
     }
     catch(error)
